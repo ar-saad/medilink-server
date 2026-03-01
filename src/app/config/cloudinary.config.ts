9bf53987af492e44f96b84a1092374d20de4ae25
ar-saad <arsaad.dev@gmail.com>
2026-03-01 17:52:26 +0600

    feat: implement file deletion from Cloudinary in global error handler if api fails in any step

--- src/app/config/cloudinary.config.ts
+++ src/app/config/cloudinary.config.ts
@@ -1,10 +1,35 @@
 import { v2 as cloudinary } from "cloudinary";
 import { env } from "./env";
+import { AppError } from "../errorHelpers/AppError";
+import status from "http-status";
 
 cloudinary.config({
   cloud_name: env.CLOUDINARY.CLOUD_NAME,
   api_key: env.CLOUDINARY.API_KEY,
   api_secret: env.CLOUDINARY.API_SECRET,
 });
+
+export const deleteFileFromCloudinary = async (url: string) => {
+  try {
+    const regex = /\/v\d+\/(.+?)(?:\.[a-zA-Z0-9]+)+$/;
+
+    const match = url.match(regex);
+
+    if (match && match[1]) {
+      const publicId = match[1];
+      await cloudinary.uploader.destroy(publicId, { resource_type: "image" });
+
+      console.log(
+        `File with public ID ${publicId} has been deleted from Cloudinary.`,
+      );
+    }
+  } catch (error) {
+    console.error("Error deleting file from Cloudinary:", error);
+    throw new AppError(
+      status.INTERNAL_SERVER_ERROR,
+      "Failed to delete file from Cloudinary",
+    );
+  }
+};
 
 export const cloudinaryUpload = cloudinary;
--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -5,15 +5,27 @@ import * as z from "zod";
 import { TErrorResponse, TErrorSources } from "../types/errorResponse.type";
 import { handleZodError } from "../errorHelpers/handleZodError";
 import { AppError } from "../errorHelpers/AppError";
+import { deleteFileFromCloudinary } from "../config/cloudinary.config";
 
-export const globalErrorHandler = (
+export const globalErrorHandler = async (
   err: any,
-  _req: Request,
+  req: Request,
   res: Response,
   _next: NextFunction,
 ) => {
   if (env.NODE_ENV === "development") {
     console.error("Global Error Handler:", err);
+  }
+
+  // If there are uploaded files in the request, attempt to delete them from Cloudinary
+  if (req.file) {
+    await deleteFileFromCloudinary(req.file.path);
+  }
+
+  if (req.files && Array.isArray(req.files) && req.files.length > 0) {
+    const imageUrls = req.files.map((file) => file.path);
+
+    await Promise.all(imageUrls.map((url) => deleteFileFromCloudinary(url)));
   }
 
   const errorSources: TErrorSources[] = [];
