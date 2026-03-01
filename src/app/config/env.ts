0760077f0dcf7d45f9e5392f7d847eba1c12defd
ar-saad <arsaad.dev@gmail.com>
2026-03-01 17:05:08 +0600

    feat: integrate Cloudinary for file uploads; add multer and cloudinary configuration

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -27,6 +27,11 @@ interface EnvConfig {
   GOOGLE_CLIENT_ID: string;
   GOOGLE_CLIENT_SECRET: string;
   GOOGLE_CALLBACK_URL: string;
+  CLOUDINARY: {
+    CLOUD_NAME?: string;
+    API_KEY?: string;
+    API_SECRET?: string;
+  };
 }
 
 const loadEnvVariables = (): EnvConfig => {
@@ -51,6 +56,9 @@ const loadEnvVariables = (): EnvConfig => {
     "GOOGLE_CLIENT_ID",
     "GOOGLE_CLIENT_SECRET",
     "GOOGLE_CALLBACK_URL",
+    "CLOUDINARY_CLOUD_NAME",
+    "CLOUDINARY_API_KEY",
+    "CLOUDINARY_API_SECRET",
   ];
 
   requiredEnvVars.forEach((variable) => {
@@ -87,6 +95,11 @@ const loadEnvVariables = (): EnvConfig => {
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID as string,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET as string,
     GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL as string,
+    CLOUDINARY: {
+      CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+      API_KEY: process.env.CLOUDINARY_API_KEY,
+      API_SECRET: process.env.CLOUDINARY_API_SECRET,
+    },
   };
 };
 
