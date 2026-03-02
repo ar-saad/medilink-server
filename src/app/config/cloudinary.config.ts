585368dc538e456adbdd2ad28287cb9c7abbf45b
ar-saad <arsaad.dev@gmail.com>
2026-03-02 10:49:07 +0600

    feat: enhance Cloudinary integration; add uploadFileToCloudinary function with error handling

--- src/app/config/cloudinary.config.ts
+++ src/app/config/cloudinary.config.ts
@@ -1,6 +1,6 @@
-import { v2 as cloudinary } from "cloudinary";
+import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
 import { env } from "./env";
-import { AppError } from "../errorHelpers/AppError";
+import { AppError, BadRequestError } from "../errorHelpers/AppError";
 import status from "http-status";
 
 cloudinary.config({
@@ -8,6 +8,60 @@ cloudinary.config({
   api_key: env.CLOUDINARY.API_KEY,
   api_secret: env.CLOUDINARY.API_SECRET,
 });
+
+export const uploadFileToCloudinary = async (
+  buffer: Buffer,
+  filename: string,
+): Promise<UploadApiResponse> => {
+  if (!buffer || !filename) {
+    throw new BadRequestError(
+      "File buffer and filename are required for upload",
+    );
+  }
+
+  const extension = filename.split(".").pop()?.toLocaleLowerCase();
+
+  const fileNameWithoutExtension = filename
+    .split(".")
+    .slice(0, -1)
+    .join(".")
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^a-z0-9\-]/g, "");
+
+  const uniqueName =
+    Math.random().toString(36).substring(2) +
+    "-" +
+    Date.now() +
+    "-" +
+    fileNameWithoutExtension;
+
+  const folder = extension === "pdf" ? "pdfs" : "images";
+
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader
+      .upload_stream(
+        {
+          resource_type: "auto",
+          public_id: `medilink/${folder}/${uniqueName}`,
+          folder: `medilink/${folder}`,
+        },
+        (error, result) => {
+          if (error) {
+            return reject(
+              new AppError(
+                status.INTERNAL_SERVER_ERROR,
+                "Failed to upload file to Cloudinary",
+              ),
+            );
+          }
+
+          resolve(result as UploadApiResponse);
+        },
+      )
+      .end(buffer);
+  });
+};
 
 export const deleteFileFromCloudinary = async (url: string) => {
   try {
