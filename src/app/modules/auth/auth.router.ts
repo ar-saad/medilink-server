d3788d4ffaf4762150402dc74a3b222537af42ce
ar-saad <arsaad.dev@gmail.com>
2026-04-03 10:19:45 +0600

    feat(auth): add request validation for register and login endpoints

--- src/app/modules/auth/auth.router.ts
+++ src/app/modules/auth/auth.router.ts
@@ -2,14 +2,20 @@ import { Router } from "express";
 import { AuthController } from "./auth.controller";
 import { checkAuth } from "../../middlewares/checkAuth";
 import { UserRole } from "../../../generated/prisma/enums";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { loginSchema, registerPatientSchema } from "./auth.schema";
 
 const router = Router();
 
 // POST | "/api/v1/auth/register" | Register a new patient
-router.post("/register", AuthController.registerPatient);
+router.post(
+  "/register",
+  validateRequest(registerPatientSchema),
+  AuthController.registerPatient,
+);
 
 // POST | "/api/v1/auth/login" | Login user
-router.post("/login", AuthController.loginUser);
+router.post("/login", validateRequest(loginSchema), AuthController.loginUser);
 
 // GET | "/api/v1/auth/me" | Get current user details
 router.get(
--- src/app/modules/auth/auth.schema.ts
+++ src/app/modules/auth/auth.schema.ts
@@ -1,13 +1,15 @@
 import * as z from "zod";
 
 export const registerPatientSchema = z.object({
-  name: z.string("Name is required"),
-  email: z.email("Invalid email address"),
+  name: z
+    .string("Name is required")
+    .min(2, "Name must be at least 2 characters long"),
+  email: z.email("Invalid email address").min(1, "Email is required"),
   password: z.string().min(6, "Password must be at least 6 characters long"),
 });
 
-export const loginUserSchema = z.object({
-  email: z.email("Invalid email address"),
+export const loginSchema = z.object({
+  email: z.email("Invalid email address").min(1, "Email is required"),
   password: z.string().min(6, "Password must be at least 6 characters long"),
 });
 
